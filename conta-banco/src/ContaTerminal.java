fccd9aceef5bd2bfddecdada98f5b7cc2e206654
Daniloel <zoltancao@gmail.com>
2024-07-03 23:34:46 -0300

    Finalizda a tarefa

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-        System.out.println("Teste classe");
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando e lendo os dados do usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner após ler o número inteiro
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem de saudação com os dados fornecidos
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo é R$" + saldo + ".");
+
+        scanner.close(); // Fechando o scanner no final da execução
+
     }
 }
